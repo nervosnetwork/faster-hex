3e1cc75c1352e604709f32162ca55bdb64544779
Xuejie Xiao <xxuejie@gmail.com>
2019-09-10 01:16:15 +0000

    fix: Do not expose hex_check_see on non-supported platform

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,12 @@ mod decode;
 mod encode;
 mod error;
 pub use crate::decode::{
-    hex_check_fallback, hex_check_sse, hex_decode, hex_decode_fallback, hex_decode_unchecked,
+    hex_check_fallback, hex_decode, hex_decode_fallback, hex_decode_unchecked,
 };
 pub use crate::encode::{hex_encode, hex_encode_fallback, hex_string, hex_to};
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64", feature = "sse4.1"))]
+pub use crate::decode::hex_check_sse;
 
 #[cfg(test)]
 mod tests {
